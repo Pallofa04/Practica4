cd10109f01971d6940f4181971e14532a345f894
Pallofa04 <115138191+Pallofa04@users.noreply.github.com>
2022-12-12 17:02:30 +0100

    Add files via upload

--- my_cat.c
+++ my_cat.c
@@ -5,19 +5,21 @@
 #define N 1024
 
 int main(){
-	char fitxer[N];
+	char name[N];
+	int fd;
 	int n;
-	printf("Introdueix el nom del fitxer\n");
-	scanf("%s",fitxer);
-	int fd = open(fitxer,O_RDONLY);
+	printf("Introdueix el nom del fitxer: ");
+	scanf("%s",name);
+	fd = open(name,O_RDONLY);
 	if (fd==-1){
-		printf("Error: El fitxer no existeix");}
-	else{
+		printf("Error: El fitxer no existeix\n");
+	}else{
 		char buf[N];
-		while ((n=read(fd, buf, N) != 0)){
+		while ((n=read(fd, buf, N))!=0){
 			buf[n]='\0';
-			printf("%s",&buf[n]);
+			printf("%s",buf);
 		}
+	close(fd);
 	}
 
 	return 0;
