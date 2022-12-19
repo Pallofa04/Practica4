36b9ec54a2040e187194acb13722d90f08ea4973
Pallofa04 <115138191+Pallofa04@users.noreply.github.com>
2022-12-19 15:43:16 +0100

    Add files via upload

--- my_cp.c
+++ my_cp.c
@@ -6,13 +6,16 @@
 
 int main(){
 	char fitxer[N];
-        printf("Introdueix el nom del fitxer\n");
+	int fd,desc,n;
+        printf("Introdueix el nom del fitxer: ");
         scanf("%s",fitxer);
-	if (int fd==-1){
+	fd = open(fitxer,O_RDONLY);
+	if (fd==-1){
                 printf("Error: El fitxer no existeix");}
         else{
-		char copia.fi[N];
-		int desc=open(copia.fi, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+		char buf[N];
+		desc=open("copia.fi", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+		while ((n=read(fd, buf, N-1))!=0) write(desc, buf, n);
 	}
 	return desc;
 }
